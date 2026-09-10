c48269bdfb3a75e74af39a93d79cf7bdb8ac630a
Marko Bušić <marko.busic@qnr.hr>
2026-09-10 22:18:55 +0200

    Initialize Spring Boot project for Contractor app, including Gradle setup and initial configuration.

---
+++ backend/src/test/java/hr/kricco/contractor/ContractorApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.kricco.contractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
